d152b4177d544305df8c079797ab0ea40dd55023
Vinlenn <zzhengaiqiang@163.com>
2021-02-20 10:00:27 +0800

    基础搭建

---
+++ src/test/java/com/vinlen/blog/ZblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinlen.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
